4f7c1122574975a847557cb069e05227b9dc721d
Maissen <maissen.developer500@gmail.com>
2025-10-13 18:51:58 +0100

    update  follows screen

--- lib/features/profile/screens/follow_screen.dart
+++ lib/features/profile/screens/follow_screen.dart
@@ -5,8 +5,18 @@ class FollowScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Followers / Following Screen')),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Navigates back to the previous screen
+          },
+        ),
+        title: const Text('Followers / Following'),
+        centerTitle: true,
+      ),
+      body: const Center(child: Text('Followers / Following Screen')),
     );
   }
 }
