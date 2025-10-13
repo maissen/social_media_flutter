b3c66d05143f1f68830b75351093f91491d1481b
Maissen <maissen.developer500@gmail.com>
2025-10-13 15:37:58 +0100

    Create login screen

---
+++ demo/lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'register_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(decoration: const InputDecoration(labelText: 'Email')),
+              const SizedBox(height: 16),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO: Navigate to main app (after login)
+                },
+                child: const Text('Login'),
+              ),
+              const SizedBox(height: 16),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const RegisterScreen(),
+                    ),
+                  );
+                },
+                child: const Text("Don't have an account? Register"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
