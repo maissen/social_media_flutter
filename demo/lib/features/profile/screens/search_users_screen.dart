0e416d4cba696ef5f591a5f2056ea84a058a8c4e
Maissen <maissen.developer500@gmail.com>
2025-10-13 15:40:34 +0100

    Create 'search users' screen

---
+++ demo/lib/features/profile/screens/search_users_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchUsersScreen extends StatelessWidget {
+  const SearchUsersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Search Users Screen')));
+  }
+}
