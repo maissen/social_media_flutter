85c28f9f412ca5b1efd3e1b85cf3916372d95393
Maissen <maissen.developer500@gmail.com>
2025-10-20 21:00:03 +0100

    update

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
-  // static const String baseApiUrl = "http://79.137.34.134:8000";
-  static const String baseApiUrl = "http://localhost:8000";
+  static const String baseApiUrl = "http://79.137.34.134:8000";
+  // static const String baseApiUrl = "http://localhost:8000";
 }
